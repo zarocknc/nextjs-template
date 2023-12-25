aeff5a0d92bc6a48391e809374b9c4bff12292e9
sebastian <hola@zarocknc.net.pe>
2023-12-25 17:26:30 -0500

    added auth

---
+++ drizzle.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from "drizzle-kit";
+import { cwd } from "node:process";
+import { loadEnvConfig } from "@next/env";
+
+loadEnvConfig(cwd());
+
+export default {
+  schema: "./src/db/**/schema.ts",
+  out: "./drizzle",
+  driver: "mysql2",
+  dbCredentials: {
+    host: process.env.DB_HOST!,
+    user: process.env.DB_USER!,
+    password: process.env.DB_PASSWORD!,
+    database: process.env.DB_NAME!,
+  },
+} satisfies Config;
